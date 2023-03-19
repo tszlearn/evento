e5519729025905a3f336b63980505b3535c12d81
Tomasz Szwarc <tsz.learn@gmail.com>
2023-03-19 21:00:24 +0100

    Update packages and create integration test for EventController

--- tests/Evento.Tests.EndToEnd/Controllers/EventControllerTests.cs
+++ tests/Evento.Tests.EndToEnd/Controllers/EventControllerTests.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Evento.Infrastructure.DTO;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace Evento.Tests.EndToEnd.Controllers
 {
-    public class EventControllerTests
+    public class EventControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
     {
+        private readonly CustomWebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public EventControllerTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task feching_events_should_return_not_empty_collection()
+        {
+            // Act
+            var response = await _client.GetAsync("events");
+            var content = await response.Content.ReadAsStringAsync();
+            var events = JsonConvert.DeserializeObject<IEnumerable<EventDto>>(content);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            events.Should().NotBeEmpty();
+        }
     }
 }
---
+++ tests/Evento.Tests.EndToEnd/CustomWebApplicationFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evento.Tests.EndToEnd
+{
+    public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+
+            builder.UseEnvironment("Development");
+        }
+    }
+}
